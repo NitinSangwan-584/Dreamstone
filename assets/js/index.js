8373de049d6f0d8a94502e2ca145902b584e975c
NitinSangwan-584 <nitinsangwan363@gmail.com>
2025-02-17 22:12:35 +0530

    footer-done

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,5 @@ const navbarbtn = () => {
     document.getElementById("name").classList.toggle("show-navbar")
     document.getElementById("rotates").classList.toggle("rotate40")
     document.getElementById("rotates2").classList.toggle("rotate-45")
+    document.body.classList.toggle("overflow-hidden")
 }
