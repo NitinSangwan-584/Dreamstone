770f08e6b169387364b1f371c569feb2524a1d3c
NitinSangwan-584 <nitinsangwan363@gmail.com>
2025-02-17 13:56:03 +0530

    navbar done

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+const navbarbtn = () => {
+    document.getElementById("name").classList.toggle("show-navbar")
+    document.getElementById("rotates").classList.toggle("rotate40")
+    document.getElementById("rotates2").classList.toggle("rotate-45")
+}
